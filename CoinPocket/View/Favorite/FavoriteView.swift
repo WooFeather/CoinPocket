bd5745860ebf8112572f6004cb013bd2e83cebca
Woohyun Jo <bluewing1004@gmail.com>
2025-04-17 17:31:38 +0900

    [Setting] 프로젝트 기본 세팅 및 폴더링

---
+++ CoinPocket/View/Favorite/FavoriteView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteView.swift
+//  CoinPocket
+//
+//  Created by 조우현 on 4/17/25.
+//
+
+import SwiftUI
+
+struct FavoriteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteView()
+}
---
+++ CoinPocket/View/Trending/TrendingView.swift
@@ -0,0 +1,18 @@
+//
+//  TrendingView.swift
+//  CoinPocket
+//
+//  Created by 조우현 on 4/17/25.
+//
+
+import SwiftUI
+
+struct TrendingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrendingView()
+}
