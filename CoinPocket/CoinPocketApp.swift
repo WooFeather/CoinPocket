5e1640129334c26936f0067fddc83228f830e115
Woohyun Jo <bluewing1004@gmail.com>
2025-04-17 17:10:55 +0900

    Initial Commit

---
+++ CoinPocket/CoinPocketApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinPocketApp.swift
+//  CoinPocket
+//
+//  Created by 조우현 on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct CoinPocketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinPocket/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CoinPocket
+//
+//  Created by 조우현 on 4/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
