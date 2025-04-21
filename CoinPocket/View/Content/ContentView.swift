43543c43aeba037db5e4dbaeaa7b608949acf91b
Woohyun Jo <bluewing1004@gmail.com>
2025-04-21 20:19:11 +0900

    [Feat] 네트워크 단절 대응

--- CoinPocket/View/Content/ContentView.swift
+++ CoinPocket/View/Content/ContentView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @StateObject private var networkMonitor = NetworkMonitor.shared
+    @State private var showAlert = false
     @State private var selectedTab: Tab = .trending
     
     var body: some View {
@@ -26,6 +28,12 @@ struct ContentView: View {
                 .asTabModifier(.profile)
         }
         .tint(.purple)
+        .onReceive(networkMonitor.$isConnected) { isConnected in
+            showAlert = !isConnected
+        }
+        .alert("네트워크 연결이 끊겼습니다. Wifi나 셀룰러 데이터를 확인해주세요.", isPresented: $showAlert) {
+            Button("확인", role: .cancel) { }
+        }
     }
 }
 
